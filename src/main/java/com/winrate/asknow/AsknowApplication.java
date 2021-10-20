08d2fa22b8354143ed87cd6e629856962e6419ee
leo.winrate <leo.winrate@kakaomobility.com>
2021-10-20 22:18:29 +0900

    Initial commit

---
+++ src/main/java/com/winrate/asknow/AsknowApplication.java
@@ -0,0 +1,13 @@
+package com.winrate.asknow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsknowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsknowApplication.class, args);
+	}
+
+}
